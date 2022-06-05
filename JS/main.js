e47f6abbed0ea8e0af8f880229cb667754e10a82
Siddharth Manjul <manjulsiddharth@gmail.com>
2022-06-05 21:50:37 +0530

    6th commit - Added About Section

--- JS/main.js
+++ JS/main.js
@@ -7,3 +7,13 @@ if (navToggle != null) {
     links.classList.toggle("show-links");
   });
 }
+
+// add fixed class to navbar
+const navbar = document.querySelector("#nav");
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
+});
